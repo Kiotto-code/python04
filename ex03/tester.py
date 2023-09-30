6bb827caf7ab208a08f0f3647e2fff4c9b74d6a9
Chan Yik Wai <yichan@student.42kl.edu.my>
2023-09-30 15:25:40 +0800

    a

--- ex03/tester.py
+++ ex03/tester.py
@@ -1,30 +1,29 @@
 from new_student import Student
 
-# Student(name='Edward', surname='agle', active=True, login='Eagle', id='asd')
-student = Student(name="Edward", surname="agle", active=False)
-# student = Student(name="Edward", surname="agle", active=True)
-# student=Student()
-print(student)
-# student = Student(name="Edward", surname="agle", active=False,
-#                   id="trannxhndgtolvh")
-# student = Student(name="Edward", surname="agle", active=False,
-#                   login="Eagle")
-# student = Student(name="Edward", surname="agle", active=False,
-#                   id="trannxhndgtolvh")
-# print(student)
+
+def main():
+    """main() docstring"""
+    student = Student(name="Edward", surname="agle")
+    print(student)
+
+    try:
+        student = Student(name="Edward", surname="agle", id="toto")
+        print(student)
+        # Student(name='Edward', surname='agle', active=True, login='Eagle', id='asd')
+        student = Student(name="Edward", surname="agle", active=False)
+        # student = Student(name="Edward", surname="agle", active=True)
+        # student=Student()
+        print(student)
+        # student = Student(name="Edward", surname="agle", active=False,
+        #                   id="trannxhndgtolvh")
+        # student = Student(name="Edward", surname="agle", active=False,
+        #                   login="Eagle")
+        # student = Student(name="Edward", surname="agle", active=False,
+        #                   id="trannxhndgtolvh")
+        # print(student)
+    except Exception as e:
+        print(e.__class__.__name__ + ":", e)
 
 
-# def main():
-#     """main() docstring"""
-#     student = Student(name="Edward", surname="agle")
-#     print(student)
-
-#     try:
-#         student = Student(name="Edward", surname="agle", id="toto")
-#         print(student)
-#     except Exception as e:
-#         print(e.__class__.__name__ + ":", e)
-
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
